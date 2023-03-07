07fbd4657ccac9e0bcbd85be58ca9bf4279d1f4a
MateoJereb <mateojereb@gmail.com>
2023-03-07 20:17:21 -0300

    Creados paquetes recycler_views, viewmodel y repo

--- app/src/main/java/com/isaiajereb/gymandgram/ui/EditarRutinaFragment.java
+++ app/src/main/java/com/isaiajereb/gymandgram/ui/EditarRutinaFragment.java
@@ -5,8 +5,11 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,11 +18,23 @@ import android.view.ViewGroup;
 import com.isaiajereb.gymandgram.R;
 import com.isaiajereb.gymandgram.databinding.FragmentEditarRutinaBinding;
 import com.isaiajereb.gymandgram.databinding.FragmentInicioBinding;
+import com.isaiajereb.gymandgram.model.Ejercicio;
+import com.isaiajereb.gymandgram.recycler_views.EjerciciosAdapter;
+import com.isaiajereb.gymandgram.repo.RutinasRepository;
+import com.isaiajereb.gymandgram.viewmodel.RutinasViewModel;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class EditarRutinaFragment extends Fragment {
 
     private FragmentEditarRutinaBinding binding;
     private NavController navController;
+    private RutinasViewModel viewModel;
+
+    private RecyclerView recyclerView;
+    private EjerciciosAdapter recyclerAdapter;
+    private List<Ejercicio> listaEjercicios;
 
     public EditarRutinaFragment() {
         // Required empty public constructor
@@ -31,6 +46,8 @@ public class EditarRutinaFragment extends Fragment {
         if (getArguments() != null) {
 
         }
+
+        viewModel = new ViewModelProvider(requireActivity()).get(RutinasViewModel.class);
     }
 
     @Override
@@ -47,5 +64,23 @@ public class EditarRutinaFragment extends Fragment {
 
         //Setear el icono de la barra de navegacion
         ((MainActivity) requireActivity()).getNavigationBar().setSelectedItemId(R.id.workout_navigation);
+
+        recyclerView = binding.ejerciciosRecyclerView;
+        recyclerAdapter = new EjerciciosAdapter(new ArrayList<Ejercicio>(),requireActivity());
+
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(requireActivity()));
+
+        listaEjercicios = RutinasRepository._EJERCICIOS;
+        recyclerAdapter.setListaEjercicios(listaEjercicios);
+        recyclerView.setAdapter(recyclerAdapter);
+
+        recyclerAdapter.setOnItemClickListener(new EjerciciosAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(Ejercicio item) {
+
+            }
+        });
+
     }
 }
