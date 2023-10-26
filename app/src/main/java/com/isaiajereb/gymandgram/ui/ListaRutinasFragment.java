3e05930224b5f01a874cd13f7aea162b4edc09d5
MateoJereb <mateojereb@gmail.com>
2023-10-26 03:23:48 -0300

    Hecho: Ordenar rutinas por campo elegido (se persiste el campo) Falta: Actualizar datos rutina actual en InicioFragment

--- app/src/main/java/com/isaiajereb/gymandgram/ui/ListaRutinasFragment.java
+++ app/src/main/java/com/isaiajereb/gymandgram/ui/ListaRutinasFragment.java
@@ -1,11 +1,19 @@
 package com.isaiajereb.gymandgram.ui;
 
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.graphics.Color;
+import android.graphics.Typeface;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.AbsoluteSizeSpan;
 import android.text.style.ForegroundColorSpan;
+import android.text.style.TypefaceSpan;
+import android.view.ContextThemeWrapper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,6 +21,7 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.res.ResourcesCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
@@ -47,6 +56,15 @@ public class ListaRutinasFragment extends Fragment {
     private RutinasAdapter adapter;
 
     private Usuario usuario;
+
+    private List<Rutina> listaRutinas;
+
+    private Integer campoDeOrdenamiento;
+    private static Integer ORDENAR_POR_NOMBRE = 0;
+    private static Integer ORDENAR_POR_FECHA_CREACION = 1;
+    private static Integer ORDENAR_POR_FECHA_MODIFICACION = 2;
+
+    private SharedPreferences sharedPreferences;
 
     public ListaRutinasFragment() {
         // Required empty public constructor
@@ -85,15 +103,22 @@ public class ListaRutinasFragment extends Fragment {
         adapter = new RutinasAdapter(new ArrayList<>(),requireActivity());
         rvRutinas.setAdapter(adapter);
 
+        sharedPreferences = getActivity().getPreferences(Context.MODE_PRIVATE);
+        campoDeOrdenamiento = sharedPreferences.getInt("campoDeOrdenamiento",ORDENAR_POR_FECHA_CREACION);
+
         //Cambiar la lista del adapter cuando se postee un valor en el live data
         viewModel.getRutinas().observe(requireActivity(), new Observer<List<Rutina>>() {
             @Override
             public void onChanged(List<Rutina> rutinas) {
-                Collections.sort(rutinas, Comparator.comparing(Rutina::getFechaCreacion));
+                listaRutinas = rutinas;
+                ordenarRutinas();
+            }
+        });
 
-                adapter.setDataRutinas(rutinas);
-                rvRutinas.setAdapter(adapter);
-                adapter.notifyDataSetChanged();
+        binding.filtrarButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialogFiltrar();
             }
         });
 
@@ -116,18 +141,7 @@ public class ListaRutinasFragment extends Fragment {
 
             @Override
             public void onEliminar(Rutina rutina) {
-                AlertDialog.Builder builder = new AlertDialog.Builder(requireActivity());
-                builder.setMessage("¿Desea eliminar '"+rutina.getNombre()+"'?")
-                        .setPositiveButton("Eliminar", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                //TODO listener eliminar
-                            }
-                        })
-                        .setNegativeButton("Cancelar", null);
-
-                AlertDialog dialog = builder.create();
-                dialog.show();
+                dialogEliminarRutina(rutina);
             }
         });
 
@@ -139,5 +153,77 @@ public class ListaRutinasFragment extends Fragment {
 
     private void onNuevaRutina(){
         navController.navigate(R.id.action_listaRutinasFragment_to_editarRutinaFragment);
+    }
+
+    private void ordenarRutinas(){
+        if(campoDeOrdenamiento == ORDENAR_POR_NOMBRE){
+            Collections.sort(listaRutinas, Comparator.comparing(Rutina::getActual).reversed().thenComparing(Rutina::getNombre));
+        }
+        else{
+            if (campoDeOrdenamiento == ORDENAR_POR_FECHA_MODIFICACION){
+                Collections.sort(listaRutinas, Comparator.comparing(Rutina::getActual).reversed().thenComparing(Rutina::getFechaUltimaModificacion));
+            }
+            else{ //Ordenar por fecha creacion
+                Collections.sort(listaRutinas, Comparator.comparing(Rutina::getActual).reversed().thenComparing(Rutina::getFechaCreacion));
+            }
+        }
+
+        adapter.setDataRutinas(listaRutinas);
+        rvRutinas.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
+    }
+
+    private void dialogEliminarRutina(Rutina rutina){
+        AlertDialog.Builder builder = new AlertDialog.Builder(requireActivity());
+        builder.setMessage("¿Desea eliminar la rutina '"+rutina.getNombre()+"'?")
+                .setPositiveButton("Eliminar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        viewModel.eliminarRutina(rutina);
+                    }
+                })
+                .setNegativeButton("Cancelar", null);
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    private void dialogFiltrar(){
+        SpannableString textoNombre = new SpannableString("Nombre"),
+                        textoFechaCreacion = new SpannableString("Fecha de creación"),
+                        textoFechaModificacion = new SpannableString("Fecha de modificación");
+
+        Typeface typeface = ResourcesCompat.getFont(requireContext(),R.font.verdana);
+        textoNombre.setSpan(new TypefaceSpan(typeface),0,textoNombre.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        textoFechaCreacion.setSpan(new TypefaceSpan(typeface),0,textoFechaCreacion.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        textoFechaModificacion.setSpan(new TypefaceSpan(typeface),0,textoFechaModificacion.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        textoNombre.setSpan(new AbsoluteSizeSpan(getResources().getDimensionPixelSize(R.dimen.mid)),0,textoNombre.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        textoFechaCreacion.setSpan(new AbsoluteSizeSpan(getResources().getDimensionPixelSize(R.dimen.mid)),0,textoFechaCreacion.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        textoFechaModificacion.setSpan(new AbsoluteSizeSpan(getResources().getDimensionPixelSize(R.dimen.mid)),0,textoFechaModificacion.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        SpannableString[] opciones = {textoNombre,textoFechaCreacion,textoFechaModificacion};
+
+        View titleView = getLayoutInflater().inflate(R.layout.dialog_filtrar_rutinas,null);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(new ContextThemeWrapper(requireActivity(),R.style.BackgroundColor));
+        builder.setCustomTitle(titleView)
+                .setSingleChoiceItems(opciones, campoDeOrdenamiento, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int selecccionado) {
+                        campoDeOrdenamiento = selecccionado;
+                        ordenarRutinas();
+
+                        SharedPreferences.Editor spEditor = sharedPreferences.edit();
+                        spEditor.putInt("campoDeOrdenamiento",campoDeOrdenamiento);
+                        spEditor.apply();
+
+                        dialog.dismiss();
+                    }
+                })
+                .setNegativeButton("Volver",null);
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
